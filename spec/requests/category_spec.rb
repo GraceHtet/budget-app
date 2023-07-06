6040ea10af78f25c2af94478f983ec539f3a2c66
Thiri <thirihtet246@gmail.com>
2023-07-06 11:09:29 +0630

    Add unit test for category

--- spec/requests/category_spec.rb
+++ spec/requests/category_spec.rb
@@ -1,31 +1,49 @@
 require 'rails_helper'
 
-RSpec.describe "Categories", type: :request do
-  describe "GET /index" do
-    it "returns http success" do
-      get "/category/index"
-      expect(response).to have_http_status(:success)
-    end
+RSpec.describe "Category", type: :request do
+  
+  include Devise::Test::IntegrationHelpers
+
+  before(:each) do
+    @user = User.create(name: 'User', email: 'user@example.com', password: 'password')
+    sign_in @user
   end
 
-  describe "GET /show" do
+  describe "GET /index" do
+
+    before(:each) do
+      get categories_path
+    end
+
     it "returns http success" do
-      get "/category/show"
       expect(response).to have_http_status(:success)
+    end
+
+    it 'get correct response status' do
+      assert_equal 'index', @controller.action_name
+    end
+
+    it 'renders an index template' do
+      assert_template :index
     end
   end
 
   describe "GET /new" do
+
+    before(:each) do
+      get new_category_path
+    end
+
     it "returns http success" do
-      get "/category/new"
       expect(response).to have_http_status(:success)
     end
-  end
 
-  describe "GET /create" do
-    it "returns http success" do
-      get "/category/create"
-      expect(response).to have_http_status(:success)
+    it 'get correct response status' do
+      assert_equal 'new', @controller.action_name
+    end
+
+    it 'renders an index template' do
+      assert_template :new
     end
   end
 
