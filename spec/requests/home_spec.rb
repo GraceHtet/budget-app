dfad929004c850b1e83c7f326c1436ca6cacebf7
Thiri <thirihtet246@gmail.com>
2023-07-06 11:08:51 +0630

    Add unit test for home

--- spec/requests/home_spec.rb
+++ spec/requests/home_spec.rb
@@ -1,7 +1,31 @@
 require 'rails_helper'
 
 RSpec.describe "Homes", type: :request do
-  describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+  include Devise::Test::IntegrationHelpers
+
+  before(:each) do
+    get home_path
+  end
+  describe 'GET /index' do
+    it 'renders a successfull response' do
+      expect(response).to have_http_status(200)
+    end
+
+    it 'get correct response status' do
+      assert_equal 'index', @controller.action_name
+    end
+
+    it 'renders an index template' do
+      assert_template :index
+    end
+
+    it 'has render a categories#index template if a user is signned in' do
+      user = User.create(name: 'User', email: 'user@example.com', password: 'password')
+      sign_in user
+
+      get home_path
+
+      expect(response).to redirect_to(categories_path)
+    end
   end
 end
