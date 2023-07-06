83483772aa7abef61b04138c2c21c70dfa43ea7b
Thiri <thirihtet246@gmail.com>
2023-07-06 11:29:51 +0630

    Add unit test for money_transaction

--- spec/requests/money_transaction_spec.rb
+++ spec/requests/money_transaction_spec.rb
@@ -1,13 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe "Money Transaction", type: :request do
-  
+
   include Devise::Test::IntegrationHelpers
 
   before(:each) do
     @user = User.create(name: 'User', email: 'user@example.com', password: 'password')
     @category = Category.create(name: 'Category',author_id: @user.id)
     sign_in @user
+
+    #attach an image to the category
+    image_path = Rails.root.join('spec', 'fixtures', 'files','healthy-food.png')
+    image = fixture_file_upload(image_path, 'image/png')
+    @category.icon.attach(io: File.open(image.path), filename: 'healthy-food.png')
   end
 
   describe "GET /index" do
